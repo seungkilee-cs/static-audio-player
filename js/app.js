e962ca67f85f935e0d606daeadaddabf087d1b25
Seung Ki Lee <seungkilee.cs@gmail.com>
2021-03-12 01:52:44 -0500

    bundled with webpack

---
+++ js/app.js
@@ -0,0 +1,18 @@
+require("./../sass/app.scss");
+
+var React = require('react/addons');
+var AudioPlayer = require("./components/AudioPlayer");
+
+var songs = [{
+	url: "../assets/Ambivalent.mp3"
+}]
+
+React.render(
+	<AudioPlayer songs={songs} />,
+	document.getElementById('audio-player1')
+);
+
+React.render(
+	<AudioPlayer dataUrl="../assets/songs.json" />, 
+	document.getElementById('audio-player2')
+);
